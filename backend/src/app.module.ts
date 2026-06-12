6e5a182e0e9e14fee4736a9347ee9c66e06a25b5
ACZH05 <alfredchin2005@gmail.com>
2026-06-12 22:04:22 +0800

    Intial verification

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { VerificationModule } from './verification/verification.module';
+import { BullModule } from '@nestjs/bullmq';
 
 @Module({
-  imports: [],
+  imports: [
+    BullModule.forRoot({
+      connection: {
+        host: process.env.REDIS_HOST ?? 'localhost',
+        port: Number(process.env.REDIS_PORT) ?? 6379,
+      },
+    }),
+    VerificationModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
