dc4b1ee484f60428726453c39a6067d373cce2f3
LEE MING DA <mdlee1919@gmail.com>
2026-06-22 14:30:31 +0800

    Revert "S2"

--- backend/src/verification/verification.module.ts
+++ backend/src/verification/verification.module.ts
@@ -1,17 +1,9 @@
 import { Module } from '@nestjs/common';
 import { VerificationService } from './verification.service';
 import { VerificationController } from './verification.controller';
-import { BullModule } from '@nestjs/bullmq';
-import { VerificationConsumer } from './verification.processor';
 
 @Module({
-  imports: [
-    BullModule.registerQueue({
-      name: 'verification',
-    }),
-  ],
   controllers: [VerificationController],
-  providers: [VerificationService, VerificationConsumer],
-  exports: [VerificationService],
+  providers: [VerificationService],
 })
 export class VerificationModule {}
